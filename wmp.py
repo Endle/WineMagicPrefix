9d50940f2c165fd62a2d27cbb03b3c1643a25edf
Li Zhenbo <litimetal@gmail.com>
2013-06-17 11:45:25 +0800

    add check_argv()

--- wmp.py
+++ wmp.py
@@ -2,3 +2,26 @@
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+
+import os
+import sys
+import shutil
+
+def check_argv():
+    """Check if the argv has only one option
+    """
+    global argv_list
+    count = 0
+    for s in argv_list:
+        if s[0] == '-':
+            count += 1
+
+    if count != 1:
+        print (argv_list)
+        raise ValueError('Wrong Command')
+#    print(argv_list)
+
+if __name__ == '__main__':
+    argv_list = sys.argv[1:]
+
+    check_argv()
