d62373d08f92a95edc599a2ebc14347ce80a5ed5
Li Zhenbo <litimetal@gmail.com>
2013-09-21 10:11:42 +0800

    Change LICENSE

--- wmp.py
+++ wmp.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# Copyright © 2013 Li Zhenbo
-# This work is free. You can redistribute it and/or modify it under the
-# terms of the Do What The Fuck You Want To Public License, Version 2,
-# as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 
 import os, sys, shutil, argparse
 
